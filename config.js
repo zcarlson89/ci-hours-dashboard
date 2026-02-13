2a1a5caaa6432bb117ea4263e4e6687e1a4fc185
Zachary Carlson <89543516+zcarlson89@users.noreply.github.com>
2026-02-13 08:24:34 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Google Sheets Configuration
+// Replace this with your Google Apps Script Web App URL
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/a/macros/katapultengineering.com/s/AKfycbwSoXUsMnpyTUFOagOEqBj2VHZn7-oUPmeB1NaMKHOpsyHtZ5m_YIzQF0rY91wMQmnjtA/exec';
+
+// This will be provided after you deploy your Google Apps Script
+// It will look like: https://script.google.com/macros/s/AKfycby.../exec
+
+window.GOOGLE_SCRIPT_URL = GOOGLE_SCRIPT_URL;
