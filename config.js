4adc93c56c6a3edc00fcae4a5cc18db46cd1a574
Zachary Carlson <89543516+zcarlson89@users.noreply.github.com>
2026-02-18 17:16:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Google Sheets Configuration
 // Replace this with your Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx_RZLvdFLeU6D1ib0e2tvOvPB83fQHBLLlVlB-NH6HZcy47lg2VjfcD6I_E_J4OLp1gA/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwkZ3xw07gE9pN8RGVxEStRwLr9AIsiIfBzeYgAu7FhH-3JyU4RpCZzJva_EhhUqgLIug/exec';
 
 // This will be provided after you deploy your Google Apps Script
 // It will look like: https://script.google.com/macros/s/AKfycby.../exec
