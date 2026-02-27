3c197c3f14f6114189a266d6430489df53fac72b
Zachary Carlson <89543516+zcarlson89@users.noreply.github.com>
2026-02-27 10:28:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Fresh simple config.js
 // Replace the URL below with your Google Apps Script Web App URL after deployment
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwPpNvgSe4Zk0j-8IXJzLj-2AdxTKvZkWGtmEutj6T5jvpS_m6ZvGGNDxpREEpWlOJG3w/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyLB1DnBePyP7x7X-b9rrWH5VQ67mwhcLW-DzlI1fM1jHsp3VuC7hf90TIJKTWJwaLbew/exec';
 
 window.GOOGLE_SCRIPT_URL = GOOGLE_SCRIPT_URL;
